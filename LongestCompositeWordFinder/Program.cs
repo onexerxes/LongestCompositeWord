aa1fa1250c7e19b6fbb954c9244a8003bb507980
onexerxes <xerxesdotiwalla@gmail.com>
2014-05-20 17:54:14 +0530

    Initial load

---
+++ LongestCompositeWordFinder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LongestCompositeWordFinderLibrary;
+
+namespace LongestCompositeWordFinder {
+	class Program {
+		// Constants
+		private static readonly string fileName = "wordsforproblem.txt";
+		
+		static void Main(string[] args) {
+			CompositeWordFinder f = new CompositeWordFinder(fileName);
+			IEnumerable<CompositeWord> compositeWords = f.GetOrderedCompositeWords();
+
+			// Spit out the results
+			IEnumerable<CompositeWord> longestCompositeWords = compositeWords.Take(2);
+			Console.WriteLine("Longest composite word: {0}", longestCompositeWords.First());
+			Console.WriteLine("Second longest composite word: {0}", longestCompositeWords.Last());
+			Console.WriteLine("Total number of composite words: {0}", compositeWords.Count());
+
+			Console.ReadKey();
+		}
+	}
+}
